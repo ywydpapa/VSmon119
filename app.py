a7f28c754db90c838cdb57ee4f76b919114b26c5
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2022-06-07 15:26:09 +0900

    지도 구성

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+import json
+from flask import Flask, request, render_template, make_response
+from time import time, localtime
+import pymysql
 
+db = None
+cur = None
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return '119 메인창 프로젝트!'
+def home():  # put application's code here
+    result= "TEST"
+    return render_template("home.html", result=result)
+
+@app.route('/map')
+def map():
+    result = "TEST"
+    return render_template("mainMap.html", result=result)
+
+@app.route('/login')
+def login():
+    result = "TEST"
+    return render_template("login.html", result=result)
 
 
 if __name__ == '__main__':

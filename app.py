a092b0f00d2194eb453bd31b25efa7d8c0fb7913
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2022-06-07 16:05:31 +0900

    DB 연결

--- app.py
+++ app.py
@@ -7,21 +7,35 @@ db = None
 cur = None
 app = Flask(__name__)
 
+def select(query):
+  # 접속정보
+  db = pymysql.connect(host='192.168.108.25', user='swcore', password='core2020', db='vs119f', charset='utf8')
+  # 커서생성
+  cur = db.cursor()
+  # 실행할 sql문
+  cur.execute(query)
+  result = cur.fetchall()
+  db.close() # 종료
+  return result
+
 @app.route('/')
 def home():  # put application's code here
-    result= "TEST"
+    sql="select * from vsUser"
+    result = select(sql)
     return render_template("home.html", result=result)
 
 @app.route('/map')
 def map():
-    result = "TEST"
+    sql="select * from vsUser"
+    result = select(sql)
     return render_template("mainMap.html", result=result)
 
 @app.route('/login')
 def login():
-    result = "TEST"
+    sql="select * from vsUser"
+    result = select(sql)
     return render_template("login.html", result=result)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=80,host='0.0.0.0')
